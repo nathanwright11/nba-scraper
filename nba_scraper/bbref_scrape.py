7e83cc0515e8fedd09b9072e4b077200d6e8f39a
nathanwright11 <nathanwright11@gmail.com>
2022-11-22 12:30:45 -0500

    Isolated player stat totals

--- nba_scraper/bbref_scrape.py
+++ nba_scraper/bbref_scrape.py
@@ -4,10 +4,18 @@ from bs4 import BeautifulSoup
 r = requests.get("https://www.basketball-reference.com/teams/GSW/2023.html#totals")
 soup = BeautifulSoup(r.content, "html.parser")
 
-#players = soup.find_all("tr")
+#isolates the player stat totals
 totals = soup.find(id="div_totals")
 
-print(totals)
+#isolates playrs and all their stats
+players = totals.find_all("td")
 
-# for player in players:
-#     print(player)
+for player in players:
+    print(player.text)
+
+#print(players[1].text)
+
+
+
+# print(totals.text)
+# print(totals.text.split())
