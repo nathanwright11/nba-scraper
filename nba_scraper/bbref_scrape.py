b83bb10440194fcbbb914dbfe0c6ff8c85dee98f
nathanwright11 <nathanwright11@gmail.com>
2022-11-21 21:35:13 -0500

    Updated bbref script

--- nba_scraper/bbref_scrape.py
+++ nba_scraper/bbref_scrape.py
@@ -3,3 +3,8 @@ from bs4 import BeautifulSoup
 
 r = requests.get("https://www.basketball-reference.com/teams/GSW/2023.html#totals")
 soup = BeautifulSoup(r.content, "html.parser")
+
+players = soup.find_all("tr")
+
+for player in players:
+    print(player)
