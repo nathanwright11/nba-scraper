6f06f228632ca3aaa2db42dea98897b7c50071a7
nathanwright11 <nathanwright11@gmail.com>
2022-11-24 16:03:19 -0500

    Writes player stats to csv file

--- nba_scraper/bbref_scrape.py
+++ nba_scraper/bbref_scrape.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
+import csv
 
 r = requests.get("https://www.basketball-reference.com/teams/GSW/2023.html")
 soup = BeautifulSoup(r.content, "html.parser")
@@ -11,18 +12,17 @@ totals = soup.find(id="totals")
 t_head = totals.find("thead")
 
 #Collects all the stat categories (with HTML tags)
-headers = t_head.find_all("th")
+headers_html = t_head.find_all("th")
 
 #Removes HTML tags and puts all categories into an array. One entry will be trash and 
 #needs to be replaced with 'name' category
-stat_ctgs = [header.text for header in headers]
+header = [header.text for header in headers_html]
 #Removes rank category
-stat_ctgs.remove("Rk")
+header.remove("Rk")
 #Replaces trash entry with 'Name' category
-stat_ctgs[0] = 'Name'
+header[0] = 'Name'
 #Converts list to array
-stat_ctgs = np.array(stat_ctgs)
-
+header = np.array(header)
 
 #Isolates the player stats
 t_body = totals.find("tbody")
@@ -38,3 +38,14 @@ player_stats = np.array(player_stats)
 #Splits up player_stats array into individual arrays of each player. Need to add a
 #way to determine/verify number of players in the table
 player_stats = np.split(player_stats, 15)
+
+#Writing the stats to a csv file
+with open('player_stats.csv', 'w', newline="") as f:
+    writer = csv.writer(f)
+    #Adds header with format (rank, name, age, stat_categories)
+    writer.writerow(header)
+    #Adds each player's stats on their own row
+    for player in player_stats:
+        writer.writerow(player)
+
+print("CSV file updated")
