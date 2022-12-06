951ee539d0665e55ee00265b06cad83b3a03d054
nathanwright11 <nathanwright11@gmail.com>
2022-12-05 19:43:19 -0500

    Created functions for grabbing stats from HTML and writing to CSV. Also added loop to grab games from multiple seasons

--- nba_scraper/player_scrape.py
+++ nba_scraper/player_scrape.py
@@ -4,56 +4,102 @@ from bs4 import BeautifulSoup
 import csv
 
 
-###Collecting Game log of Players###
+######-------------------Game Log Collection Steps------------------######
+#
+#    1. Create url of season to grab game logs
+#
+#    2. Grab html of that seasons' game log
+#    
+#    3. Isolate regular season game stats
+#
+#    4. Grab stats for all 82 games (stats/INACTIVE)
+#
+#    5. Write stats to csv
+#
+######--------------------------------------------------------------######
+
+
+#takes result from GET request, isolates regular season stats, puts all game 
+#stats into array
+def get_game_stats(url_request):
+    
+    #parsers html of url_request
+    soup = BeautifulSoup(url_request.content, "html.parser")
+
+    #Isolates regular season stats
+    reg_stat = soup.find("table", id="pgl_basic")
+
+    #Isolates stat categories
+    thead = reg_stat.find("thead")
+    #Grabs stats categories
+    stat_html = thead.find_all("th")
+
+    stat_ctgs = []
+    for stat in stat_html:
+        stat_ctgs.append(stat.text)
+
+    #Isolates game stats
+    tbody = reg_stat.find("tbody")
+    games = tbody.find_all("tr")
+
+    #An array of arrays. Each game will be an array of their own stats, combined into
+    #the game_logs array
+    game_logs = []
+
+    #Loops through each game and pulls all stats/INACTIVE status
+    for game in games:
+        stats = []
+        #Pulls all possible stats from game (accounts for INACTIVE status)
+        game_stats = game.find_all("td")
+
+        #Puts each stat into array as their own item
+        for stat in game_stats:
+            stats.append(stat.text)
+    
+        #Adds the stats array to the game_log array so each game is their own array
+        game_logs.append(stats)
+    
+    return stat_ctgs, game_logs
+
+
+#function to write stats from a given season to csv file
+def write_csv(header, game_logs, year):
+    
+    with open(f'curry_games_{year}.csv', 'w', newline="") as f:
+        writer = csv.writer(f)
+        #Adds header with format (rank, name, age, stat_categories)
+        writer.writerow(header)
+        #Adds each player's stats on their own row
+        for game in game_logs:
+            writer.writerow(game)
+
+    return print(f"curry_games_{year}.csv updated")
+
+
+
+
+######----------Main Section-----------------#####
+
+
+#starting year to grab games
+year = 2023
 
 #URL for Steph Curry game logs
-curry_url = "https://www.basketball-reference.com/players/c/curryst01/gamelog/2022"
+curry_url = "https://www.basketball-reference.com/players/c/curryst01/gamelog/"
 
-r = requests.get(curry_url)
-soup = BeautifulSoup(r.content, "html.parser")
+#Number of previous years to grab
+prev_yrs = 5
 
-#Isolates regular season stats
-reg_stat = soup.find("table", id="pgl_basic")
+#Loop to grab games from multiple seasons
+for i in range(prev_yrs):
 
-#Isolates stat categories
-thead = reg_stat.find("thead")
-#Grabs stats categories
-stat_html = thead.find_all("th")
+    #GET request to grab website data
+    r = requests.get(curry_url + str(year))
 
-stat_ctgs = []
-for stat in stat_html:
-    stat_ctgs.append(stat.text)
+    #Grabs stat categories and game stats
+    header, game_logs = get_game_stats(r)
 
-#Isolates game stats
-tbody = reg_stat.find("tbody")
-games = tbody.find_all("tr")
+    #Writes game stats to csv
+    write_csv(header, game_logs, year)
 
-#An array of arrays. Each game will be an array of their own stats, combined into
-#the game_logs array
-game_log = []
-
-#Loops through each game and pulls all the stats or INACTIVE status
-for game in games:
-    #Created inside loop so it is empty before the start of every game
-    stats = []
-    #Pulls all possible stats from game (accounts for INACTIVE status)
-    game_stats = game.find_all("td")
-
-    #Puts each stat into array as their own item
-    for stat in game_stats:
-        stats.append(stat.text)
-    
-    #Adds the stats array to the game_log array so each game is their own array
-    game_log.append(stats)
-
-
-#Writing the stats to a csv file
-with open('curry_games.csv', 'w', newline="") as f:
-    writer = csv.writer(f)
-    #Adds header with format (rank, name, age, stat_categories)
-    writer.writerow(stat_ctgs)
-    #Adds each player's stats on their own row
-    for game in game_log:
-        writer.writerow(game)
-
-print("CSV file updated")
+    year -= 1
